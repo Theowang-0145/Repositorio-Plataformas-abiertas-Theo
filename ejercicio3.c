fe6ddd1dd00d4ec3d732d1cebb3f675f8f6f7870
Theo Wang <th.trabajos.wang@gmail.com>
2026-05-21 01:00:24 +0000

    Segundo avance: Implementacion de funcion de contar estados de los sensores

--- ejercicio3.c
+++ ejercicio3.c
@@ -31,8 +31,22 @@ EstadoSensor clasificar(float valor){           //Esta funcion devuelve un estad
     }
     return estado;
 }
-int contar_estado(Lectura s[], int n, EstadoSensor e);
-int lectura_extrema(Lectura s[], int n);
+int contar_estado(Lectura sensor[], int n, EstadoSensor estado_t){      //una vez inicializadas solo queda utiliz
+    int i, contador; 
+    contador = 0;
+
+    for (i = 0; i < n ; i++){           //lo que realiza este for es igualar el valor enum de los sensores al indicado en los parametros 
+        if (sensor[i].estado == estado_t){
+            contador++;     //despues se guarda el numeros de veces que entra en ese condicional
+        }
+    
+    }
+    return contador;        //por ultimo se retorna el contador
+
+}
+int lectura_extrema(Lectura s[], int n){
+    int i; 
+}
  
 /*
 =================================================== SECCION DE MAIN =====================================================
@@ -59,9 +73,14 @@ int main (void) {
     for (i = 0; i < MAX_SENSORES; i++){             //se utiliza otra vez un ciclo for para imprimir los demas datos de manera seguida 
         printf("|-%d-|-%.2f-|-%s|\n",sensores[i].id,sensores[i].valor,
         (sensores[i].estado == NORMAL) ? "NORMAL" :         //con este condicional se imprimen los datos obtenidos por el valor
-        (sensores[i].estado == ALERTA) ? "ALERTA" :         //resulta necesario para poder pasar el enum a algo imprimible.
-        "FALLO");
+        (sensores[i].estado == ALERTA) ? "ALERTA" : "FALLO");  //resulta necesario para poder pasar el enum a algo imprimible.
+        
     }
+    
+    printf("\nConteo de estados \n");       //como lo unico que retorna la funcion es un numero se puede implementar directamente en el printf
+    printf("El conteo de senosres en estado Normal es: %d \n", contar_estado(sensores,MAX_SENSORES,NORMAL));
+    printf("El conteo de senosres en estado de ALERTA es: %d \n", contar_estado(sensores,MAX_SENSORES,ALERTA));
+    printf("El conteo de senosres en estado de FALLO es: %d \n", contar_estado(sensores,MAX_SENSORES,FALLO));
 
 
     return 0; 
